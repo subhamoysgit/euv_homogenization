8d6ca6ddabf59325ddf8e319d083fde23eed90c1
subhamoysgit <sc8473@gmail.com>
2022-03-03 12:05:10 -0700

    made reg. weight and var independent, reduced reg. weightage, reduce number of train, val patches, increased #epochs and modified checkpoint to save each epoch and loss, mse history

--- experiments/abae_uv/abae_memorization.py
+++ experiments/abae_uv/abae_memorization.py
@@ -64,7 +64,7 @@ sys.path.append(_SRC_DIR)
 import numpy as np
 import tensorflow as tf
 from keras.callbacks import ModelCheckpoint
-
+import pickle
 
 ##------------------------------------------------------------------------------------
 ## Random seed initialization
@@ -84,9 +84,9 @@ EPOCH0 = 1  # First epoch
 
 DATA_NOISE = 0.1 # noise variance as mean of aia patch hist
 W_VAR_I = 1.0 # variance of the anchor weights
-W_LAMBDA_I = DATA_NOISE/W_VAR_I # Strength of the regularization term for anchor weights
+W_LAMBDA_I = 0.01 # Strength of the regularization term for anchor weights
 B_VAR_I = W_VAR_I # variance of the anchor biases 
-B_LAMBDA_I = DATA_NOISE/B_VAR_I # Strength of the regularization term for anchor biases
+B_LAMBDA_I = 0.01 # Strength of the regularization term for anchor biases
 
 
 ##------------------------------------------------------------------------------------
@@ -107,9 +107,9 @@ optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001,beta_1=0.5)
 
 
 OUTPUT_FOLDER = '/d0/models/'
-OUTPUT_FILE = 'eit_aia_sr_abae_small_R_L_10_'  #(R = ratio of Data Noise and Variance) L_10 is Ratio = 1/10
-TRAIN_DATE_RANGE = [20140101,20141231]
-VAL_DATE_RANGE = [20160101,20160229]
+OUTPUT_FILE = 'eit_aia_sr_abae_small_LAMBDA_01_VAR_1_'
+TRAIN_DATE_RANGE = [20140101,20140430]
+VAL_DATE_RANGE = [20160101,20160131]
 
 if __name__ == "__main__":
 
@@ -126,5 +126,9 @@ if __name__ == "__main__":
 
 	for m in range(ENSEMBLE_SIZE):
 		print('-- training: ' + str(m+1) + ' of ' + str(ENSEMBLE_SIZE) + ' CNNs --') 
-		checkpoint = ModelCheckpoint(OUTPUT_FOLDER + OUTPUT_FILE + str(m+1).zfill(2) + '.h5', monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=True, mode='auto', save_freq='epoch')
-		history = CNNs[m].fit(imageLoader(TRAIN_PATH, BATCH_SIZE, DateRange = TRAIN_DATE_RANGE, rng=rng, vflip=VFLIP, hflip=HFLIP), batch_size = ((VFLIP+HFLIP)**2)*BATCH_SIZE, steps_per_epoch = nTrain//BATCH_SIZE, epochs = 5, callbacks=[checkpoint], validation_data=imageLoader(VAL_PATH, BATCH_SIZE, DateRange = VAL_DATE_RANGE, rng=rng, vflip=VFLIP, hflip=HFLIP), validation_steps=nVal//BATCH_SIZE, initial_epoch=EPOCH0-1)
+		checkpoint = ModelCheckpoint(OUTPUT_FOLDER + OUTPUT_FILE + str(m+1).zfill(2) +'_'+'{epoch:02d}.h5', monitor='val_loss', verbose=1, save_weights_only=True, mode='auto', save_freq='epoch')
+		history = CNNs[m].fit(imageLoader(TRAIN_PATH, BATCH_SIZE, DateRange = TRAIN_DATE_RANGE, rng=rng, vflip=VFLIP, hflip=HFLIP), batch_size = ((VFLIP+HFLIP)**2)*BATCH_SIZE, steps_per_epoch = nTrain//BATCH_SIZE, epochs = 10, callbacks=[checkpoint], validation_data=imageLoader(VAL_PATH, BATCH_SIZE, DateRange = VAL_DATE_RANGE, rng=rng, vflip=VFLIP, hflip=HFLIP), validation_steps=nVal//BATCH_SIZE, initial_epoch=EPOCH0-1)
+		pickle.dump(history.history['loss'],open(OUTPUT_FOLDER + OUTPUT_FILE + str(m+1).zfill(2) +'_'+'loss.p','wb'))
+		pickle.dump(history.history['val_loss'],open(OUTPUT_FOLDER + OUTPUT_FILE + str(m+1).zfill(2) +'_'+'val_loss.p','wb'))
+		pickle.dump(history.history['mse'],open(OUTPUT_FOLDER + OUTPUT_FILE + str(m+1).zfill(2) +'_'+'mse.p','wb'))
+		pickle.dump(history.history['val_mse'],open(OUTPUT_FOLDER + OUTPUT_FILE + str(m+1).zfill(2) +'_'+'val_mse.p','wb'))
