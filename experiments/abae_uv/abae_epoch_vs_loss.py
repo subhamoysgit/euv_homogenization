d41c752278717a5dbd749c7ea2f1d9cbd0bf25d9
subhamoysgit <sc8473@gmail.com>
2022-03-08 14:36:48 -0700

    modified to code to create a single plot with shwoing the loss history for all the var and ambda combinations

--- experiments/abae_uv/abae_epoch_vs_loss.py
+++ experiments/abae_uv/abae_epoch_vs_loss.py
@@ -45,36 +45,46 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 OUTPUT_FOLDER = '/d0/models/'
-OUTPUT_FILE = 'eit_aia_sr_abae_small_LAMBDA_01_VAR_1_'
-ENSEMBLE_SIZE = 7
+OUTPUT_FILES = ['eit_aia_sr_abae_small_LAMBDA_01_VAR_1_','eit_aia_sr_abae_small_LAMBDA_01_VAR_d1_','eit_aia_sr_abae_small_LAMBDA_0001_VAR_d1_']
+PARAMS = ['(V = 1, L = 0.01)','(V = 0.1, L = 0.01)','(V = 0.1, L = 0.0001)']
+COLORS = [['k','r'],['g','b'],['#E69F00','#56B4E9']]
+ENSEMBLE_SIZE = 4
 
 if __name__ == "__main__":
-	loss = np.zeros((7,10))
-	val_loss = np.zeros((7,10))
-	mse = np.zeros((7,10))
-	val_mse = np.zeros((7,10))
-	for m in range(ENSEMBLE_SIZE):
-		t = pickle.load(open(OUTPUT_FOLDER + OUTPUT_FILE + str(m+1).zfill(2) +'_'+'loss.p','rb'))
-		loss[m,:] = t
-		v = pickle.load(open(OUTPUT_FOLDER + OUTPUT_FILE + str(m+1).zfill(2) +'_'+'val_loss.p','rb'))
-		val_loss[m,:] = v
-		tm = pickle.load(open(OUTPUT_FOLDER + OUTPUT_FILE + str(m+1).zfill(2) +'_'+'mse.p','rb'))
-		mse[m,:] = tm
-		vm = pickle.load(open(OUTPUT_FOLDER + OUTPUT_FILE + str(m+1).zfill(2) +'_'+'val_mse.p','rb'))
-		val_mse[m,:] = vm
+	loss = np.zeros((len(OUTPUT_FILES),ENSEMBLE_SIZE,10))
+	val_loss = np.zeros((len(OUTPUT_FILES),ENSEMBLE_SIZE,10))
+	mse = np.zeros((len(OUTPUT_FILES),ENSEMBLE_SIZE,10))
+	val_mse = np.zeros((len(OUTPUT_FILES),ENSEMBLE_SIZE,10))
+	for n in range(len(OUTPUT_FILES)):
+		for m in range(ENSEMBLE_SIZE):
+			t = pickle.load(open(OUTPUT_FOLDER + OUTPUT_FILES[n] + str(m+1).zfill(2) +'_'+'loss.p','rb'))
+			loss[n,m,:] = t
+			v = pickle.load(open(OUTPUT_FOLDER + OUTPUT_FILES[n] + str(m+1).zfill(2) +'_'+'val_loss.p','rb'))
+			val_loss[n,m,:] = v
+			tm = pickle.load(open(OUTPUT_FOLDER + OUTPUT_FILES[n] + str(m+1).zfill(2) +'_'+'mse.p','rb'))
+			mse[n,m,:] = tm
+			vm = pickle.load(open(OUTPUT_FOLDER + OUTPUT_FILES[n] + str(m+1).zfill(2) +'_'+'val_mse.p','rb'))
+			val_mse[n,m,:] = vm
+
 	epochs = np.linspace(1,10,10)
 	plt.subplot(1,2,1)
-	plt.fill_between(epochs,loss.min(axis=0),loss.max(axis=0),color='k',alpha=0.5,label='train')
-	plt.fill_between(epochs,val_loss.min(axis=0),val_loss.max(axis=0),color='r',alpha=0.5,label='val')
+	for n in range(len(OUTPUT_FILES)):
+		plt.fill_between(epochs,loss[n,:,:].min(axis=0),loss[n,:,:].max(axis=0),color=COLORS[n][0],alpha=0.5,label='train '+PARAMS[n])
+		plt.fill_between(epochs,val_loss[n,:,:].min(axis=0),val_loss[n,:,:].max(axis=0),color=COLORS[n][1],alpha=0.5,label='val '+PARAMS[n])
 	plt.ylabel('total loss')
 	plt.xlabel('epochs')
+	plt.ylim([val_mse.min(),loss.max()])
+	#plt.text(4,10**12,'V = 1, L = 0.01')
 	plt.yscale('log')
 	plt.legend(frameon = False)
 	plt.subplot(1,2,2)
-	plt.fill_between(epochs,mse.min(axis=0),mse.max(axis=0),color='k',alpha=0.5,label='train')
-	plt.fill_between(epochs,val_mse.min(axis=0),val_mse.max(axis=0),color='r',alpha=0.5,label='val')
+	for n in range(len(OUTPUT_FILES)):
+		plt.fill_between(epochs,mse[n,:,:].min(axis=0),mse[n,:,:].max(axis=0),color=COLORS[n][0],alpha=0.5,label='train '+PARAMS[n])
+		plt.fill_between(epochs,val_mse[n,:,:].min(axis=0),val_mse[n,:,:].max(axis=0),color=COLORS[n][1],alpha=0.5,label='val '+PARAMS[n])
 	plt.ylabel('mse')
 	plt.xlabel('epochs')
+	plt.ylim([val_mse.min(),loss.max()])
+	#plt.text(4,10**12,'V = 1, L = 0.01')
 	plt.yscale('log')
 	plt.legend(frameon = False)
 	plt.show()
