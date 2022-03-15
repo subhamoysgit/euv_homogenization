e329f0da14bf59a109a71e8dc7d2c9b7d81f23c3
subhamoysgit <sc8473@gmail.com>
2022-03-15 11:11:14 -0600

    lambda = 0.1

--- experiments/abae_uv/m03-22_abae_memorization_glorot_init.py
+++ experiments/abae_uv/m03-22_abae_memorization_glorot_init.py
@@ -85,9 +85,9 @@ EPOCH0 = 1  # First epoch
 
 DATA_NOISE = 0.1 # noise variance as mean of aia patch hist
 W_ST_I = 1.0 # variance of the anchor weights
-W_LAMBDA_I = 1.0 # Strength of the regularization term for anchor weights
+W_LAMBDA_I = 0.1 # Strength of the regularization term for anchor weights
 B_ST_I = 1.0 # variance of the anchor biases 
-B_LAMBDA_I = 1.0 # Strength of the regularization term for anchor biases
+B_LAMBDA_I = 0.1 # Strength of the regularization term for anchor biases
 
 
 ##------------------------------------------------------------------------------------
@@ -108,7 +108,7 @@ optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001,beta_1=0.5)
 
 
 OUTPUT_FOLDER = '/d0/models/'
-OUTPUT_FILE = 'eit_aia_sr_abae_small_GLOROT_UNIF_LAMBDA_1_ST_1_'
+OUTPUT_FILE = 'eit_aia_sr_abae_small_GLOROT_UNIF_LAMBDA_d1_ST_1_'
 TRAIN_DATE_RANGE = [20140101,20140228]
 VAL_DATE_RANGE = [20160101,20160115]
 
